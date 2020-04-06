1ea1c0b17186b46c84417611f4b01dc09355e863
Ricardo Sgobbe <ricardo.sgobbe@arctouch.com>
2020-04-06 16:00:33 -0300

    Initial Commit

---
+++ CustomCarousel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCarousel
+//
+//  Created by Ricardo Sgobbe on 06/04/20.
+//  Copyright © 2020 Ricardo Sgobbe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
